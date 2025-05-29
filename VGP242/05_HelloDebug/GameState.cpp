0c731b5cac07c6b4c63e7639a41776e29dd43726
Narda05 <153568349+Narda05@users.noreply.github.com>
2025-05-28 23:44:26 -0700

    Hello Debug

--- VGP242/05_HelloDebug/GameState.cpp
+++ VGP242/05_HelloDebug/GameState.cpp
@@ -53,9 +53,12 @@ const char* gShapeNames[] =
 	"Transform",
 };
 
+bool gShapeVisible[] = { false, false, false, false, false, false, false };
+Math::Vector3 gTransformPosition = Math::Vector3::Zero;
 Shape gCurrentShape = Shape::None;
 void GameState::DebugUI() 
 {
+	
 	ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
 	ImGui::Text("Hello Yall");
 	ImGui::Checkbox("IsChecked", &gCheValue);
@@ -71,49 +74,65 @@ void GameState::DebugUI()
 		gCurrentShape = (Shape)currentShape;
 	}
 
-	switch (gCurrentShape)
+	if (gCurrentShape != Shape::None)
 	{
-	case Shape::None: break;
-	case Shape::AABB: 
-	{
-		/*ImGui::DragFloat("Min");
-		ImGui::DragFloat("Max");*/
-		SimpleDraw::AddAABB(gV0, gV1, gColor);
-		break;
+		ImGui::Checkbox("Show Shape", &gShapeVisible[currentShape]);
 	}
-	case Shape::AABBFilled: 
+
+	if (gCurrentShape == Shape::Transform)
 	{
-		/*ImGui::DragFloat("Min");
-		ImGui::DragFloat("Max");*/
-		SimpleDraw::AddFilledAABB(gV0, gV1, gColor);
-		break;
+		ImGui::DragFloat3("Transform Position", &gTransformPosition.x, 0.1f);
 	}
-	case Shape::Sphere:
-	{
-		/*ImGui::DragFloat("Min");
-		ImGui::DragFloat("Max");*/
-		SimpleDraw::AddSphere(60, 60, gFloatVal, gColor, gV0);
-		break;
-	}
-	case Shape::GroundPlane: 
-	{
-		/*ImGui::DragFloat("Min");
-		ImGui::DragFloat("Max");*/
-		SimpleDraw::AddGroundPlane(gFloatVal, gColor);
-		break;
-	}
-	case Shape::GroundCircle:
-	{
-		/*ImGui::DragFloat("Min");
-		ImGui::DragFloat("Max");*/
-		SimpleDraw::AddGroundCircle(60, gFloatVal, gColor, gV0);
-		break;
-	}
-	case Shape::Transform: 
-	{
-		SimpleDraw::AddTransform(Math::Matrix4::Identity);
-		break;
-	}
+
+	if (gCurrentShape != Shape::None && gShapeVisible[currentShape])
+	{ 
+		switch (gCurrentShape)
+		{
+		case Shape::None: break;
+		case Shape::AABB: 
+		{
+			ImGui::DragFloat3("Min", &gV0.x, 0.1f);
+			ImGui::DragFloat3("Max", &gV1.x, 0.1f);
+			SimpleDraw::AddAABB(gV0, gV1, gColor);
+			break;
+		}
+		case Shape::AABBFilled: 
+		{
+			ImGui::DragFloat3("Min", &gV0.x, 0.1f);
+			ImGui::DragFloat3("Max", &gV1.x, 0.1f);
+			SimpleDraw::AddFilledAABB(gV0, gV1, gColor);
+			break;
+		}
+		case Shape::Sphere:
+		{
+			/*ImGui::DragFloat("Min");
+			ImGui::DragFloat("Max");*/
+			SimpleDraw::AddSphere(60, 60, gFloatVal, gColor, gV0);
+			break;
+		}
+		case Shape::GroundPlane: 
+		{
+			/*ImGui::DragFloat("Min");
+			ImGui::DragFloat("Max");*/
+			SimpleDraw::AddGroundPlane(gFloatVal, gColor);
+			break;
+		}
+		case Shape::GroundCircle:
+		{
+			/*ImGui::DragFloat("Min");
+			ImGui::DragFloat("Max");*/
+			SimpleDraw::AddGroundCircle(60, gFloatVal, gColor, gV0);
+			break;
+		}
+		case Shape::Transform: 
+		{
+			Math::Matrix4 transform = Math::Matrix4::Translation(gTransformPosition);
+			SimpleDraw::AddTransform(Math::Matrix4::Identity);
+			break;
+		}
+		default:
+			break;
+		}
 	}
 	ImGui::End();
 	//SimpleDraw::AddFace( gV0, gV1, gV2, Colors::AliceBlue);
